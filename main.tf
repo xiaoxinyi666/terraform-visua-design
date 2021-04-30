55fd653284cbaecabe4fbca35ee58b0f11a0eadd
root <xiaoxinyi@chinasofti.com>
2021-04-30 03:13:16 +0000

    Fri Apr 30 2021 - Committed via Codeherent

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public_sunnet1_az_a" {
   cidr_block = 10.0.101.0
   vpc_id = aws_vpc.vpc.id
-  availability_zone_id = data.aws_availability_zones.availability_zones
 }
 
 resource "aws_subnet" "subnet1" {
@@ -26,4 +25,21 @@ resource "aws_subnet" "subnet2" {
 resource "aws_subnet" "subnet3" {
   cidr_block = 
   vpc_id = 
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = ecs_cluster
+  capacity_providers = [
+    fargate,
+    ,
+  ]
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name = 
+}
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  container_definitions = 
+  family = 
 }
