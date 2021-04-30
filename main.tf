997d7336c89a84edc49c1e70074d8f9e3e76827b
root <xiaoxinyi@chinasofti.com>
2021-04-30 02:42:09 +0000

    Fri Apr 30 2021 - Committed via Codeherent

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 provider "aws" {
-  region  = "ap-northeast-1"
+  region = "ap-northeast-1"
   version = "~> 2.57"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = 10.0.0.0/16
+}
+
+resource "aws_subnet" "subnet" {
+  cidr_block = 10.0.101.0/24
+  vpc_id = aws_vpc.vpc
 }
